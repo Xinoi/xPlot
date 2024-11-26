59a5b59407788c118d818dce2f8993b92414d2be
Johannes Zaunrith <zaunrithj@gmail.com>
2024-11-26 21:35:21 +0100

    Input Iteratin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod input;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("-- input function (only +, -, /, * and Brackets are supported) --\n-- Use x as the variable! --");
+    while let Err(error) = input::get_input() {
+        println!("{}", error);
+    } 
 }
+
